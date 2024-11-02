d9db42878b0a4852f1441287e19625907fa3dd83
0ZeYaD0 <Z.Soliman01807@student.aast.edu>
2024-11-02 10:33:57 +0200

    done

---
+++ Project/Random number game.py
@@ -0,0 +1,33 @@
+import random
+
+lowest_number =  1
+highest_number = 100
+
+answer = random.randint(lowest_number, highest_number)
+guesses = 0
+is_running = True
+
+print("Welcome to number guessing game")
+print(f"Select a number between {lowest_number} and {highest_number}")
+
+while is_running:
+
+    guess = input("Enter your guess: ")
+    if guess.isdigit():
+        guess = int(guess)
+        guesses += 1
+
+        if guess < lowest_number or guess > highest_number:
+            print(f"The number {guess} is out of range")
+        elif guess < answer:
+            print(f"The number {guess} is less than the answer")
+        elif guess > answer:
+            print(f"The number {guess} is bigger than the answer")
+        else:
+            print(f"Correct the answer is {answer}")
+            print(f"Your guesses is {guesses}")
+            is_running = False
+    else:
+        print("Invalid input")
+        print(f"Please select a number between {lowest_number} and {highest_number}")
+    
---
+++ Project/concession stand.py
@@ -0,0 +1,30 @@
+menu = {"pizza":5.00,
+        "s popcorn": 2.50,
+        "m popcorn": 3.50,
+        "l popcorn":4.00,
+        "nacho":3.00,
+        "coke":1.50,
+        "fries": 4.50,
+        "chips":1.00,
+        "lemonade":1.50}
+
+cart = []
+total = 0
+
+print("----------------- MENU -----------------")
+for key,value in menu.items():
+    print(f"{key:10}: ${value:.2f}")
+print("----------------------------------------")
+
+while True:
+    food = input("Select what do you want (q to quit):").lower()
+    if food == 'q':
+        break
+    elif menu.get(food) is not None:
+        cart.append(food)
+print("----------------- YOUR ORDER -----------------")
+for food in cart:
+    total += menu.get(food)
+    print(food,end=" ")
+
+print(f"The total is: ${total:.2f} ")
